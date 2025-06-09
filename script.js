7fa13299c53038a7fcaaa803fb55c3adde0cbce6
John Wesley <wesleyjohn691@gmail.com>
2025-06-09 14:37:29 +0300

    Create media queries for mobile compatibility. Create Accordion logic for the navbar

--- script.js
+++ script.js
@@ -12,3 +12,12 @@
       item.classList.toggle('active');
     });
   });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById('hamburger');
+  const navLinks = document.getElementById('nav-links');
+
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+  });
+});
